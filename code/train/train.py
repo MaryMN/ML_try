b23d6e9c2ebda6cbc75013d763249b8418a764a6
MaryMN <42991909+MaryMN@users.noreply.github.com>
2021-03-01 01:25:49 +0200

    Update train.py

--- code/train/train.py
+++ code/train/train.py
@@ -1,79 +1,9 @@
-# import os
-# import argparse
-# import itertools
-# import joblib
-# import matplotlib.pyplot as plt
-
-
-# from azureml.core import Dataset, Run
-
-# from sklearn import datasets
-# from sklearn.svm import SVC
-# from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
-
-# import pandas as pd
-# import numpy as np
-# from sklearn.linear_model import LinearRegression
-# from sklearn.model_selection import train_test_split
-# from sklearn import metrics
-
-
-# run = Run.get_context()
-
-# def main(args):
-#     # create the outputs folder
-#     os.makedirs('outputs', exist_ok=True)
-    
-#     # Log arguments
-#    datasset = pd.read_csv('data.txt')
-
-
-#     datasset.shape
-#     datasset.head()
-#     datasset.describe()
-
-#     X = datasset.iloc[:, :-1].values
-#     y = datasset.iloc[:, 8].values
-
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-#     regressor = LinearRegression()
-#     regressor.fit(X_train, y_train)
-
-#     print(regressor.intercept_)
-
-#     print(regressor.coef_)
-
-#     y_pred = regressor.predict(X_test)
-
-#     df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-#     df
-#     print(df.head())
-
-
-#     print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-#     print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-#     print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-    
-#     # files saved in the "outputs" folder are automatically uploaded into run history
-#     model_file_name = "model.pkl"
-#     joblib.dump(svm_model, os.path.join('outputs', model_file_name))
-
-
-# def parse_args():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--kernel', type=str, default='rbf', help='Kernel type to be used in the algorithm')
-#     parser.add_argument('--penalty', type=float, default=1.0, help='Penalty parameter of the error term')
-#     args = parser.parse_args()
-#     return args
-
-
-# if __name__ == '__main__':
-#     args = parse_args()
-#     main(args=args)
+  
 import os
 import argparse
 import itertools
+import numpy as np
+import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
 
@@ -83,62 +13,116 @@ from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_
 from sklearn.model_selection import train_test_split
 
 from azureml.core import Dataset, Run
-
-
-
-
-
-
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn import preprocessing
-from sklearn import metrics
-
-
-
-
 run = Run.get_context()
+
+
+def log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix', title='Confusion matrix', cmap=plt.cm.Blues):
+    '''
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    '''
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
+        print('Normalized confusion matrix')
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    
+    plt.figure()
+    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation = 45)
+    plt.yticks(tick_marks, labels)
+    
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment = "center", color = 'white' if cm[i, j] > thresh else 'black')
+    
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    run.log_image(log_name, plot=plt)
+    plt.savefig(os.path.join('outputs', '{0}.png'.format(log_name)))
+
+
+def log_confusion_matrix(cm, labels):
+    # log confusion matrix as object
+    cm_json =   {
+       'schema_type': 'confusion_matrix',
+       'schema_version': 'v1',
+       'data': {
+           'class_labels': labels,
+           'matrix': cm.tolist()
+       }
+    }
+    run.log_confusion_matrix('confusion_matrix', cm_json)
+    
+    # log confusion matrix as image
+    log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix_unnormalized', title='Confusion matrix')
+    
+    # log normalized confusion matrix as image
+    log_confusion_matrix_image(cm, labels, normalize=True, log_name='confusion_matrix_normalized', title='Normalized confusion matrix')
 
 
 def main(args):
     # create the outputs folder
     os.makedirs('outputs', exist_ok=True)
+    
+    # Log arguments
+    run.log('Kernel type', np.str(args.kernel))
+    run.log('Penalty', np.float(args.penalty))
 
-    data_old = pd.read_csv('apartmentComplexData.txt', sep=",", header=None)
-    print(data_old.head())
+    # Load iris dataset
+    X, y = datasets.load_iris(return_X_y=True)
+    
+    #dividing X,y into train and test data
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=223)
+    data = {'train': {'X': x_train, 'y': y_train},
+            'test': {'X': x_test, 'y': y_test}}
+    
+    # train a SVM classifier
+    svm_model = SVC(kernel=args.kernel, C=args.penalty, gamma='scale').fit(data['train']['X'], data['train']['y'])
+    svm_predictions = svm_model.predict(data['test']['X'])
 
-    data = preprocessing.normalize(data_old)
-    data = pd.DataFrame(data)
-    print(data.head())
-
-    X = data.iloc[:, [2,3,4,5,6]]
-    y = data.iloc[:, 8]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-
-    linear_model = LinearRegression()
-    linear_model.fit(X_train, y_train)
-    y_predicted = linear_model.predict(X_test)
-
-    df = pd.DataFrame({'Actual': y_test, 'Predicted': y_predicted})
-    print(df.head())
-    print('Mean absolute error: ',metrics.mean_absolute_error(y_test, y_predicted))
-    print('Mean squared error: ', metrics.mean_squared_error(y_test, y_predicted))
-    print('Mean squared root error: ', np.sqrt(metrics.mean_squared_error(y_test, y_predicted)))
-    print("Training set {:.2f}".format(linear_model.score(X_train, y_train)))
-    print("Test set {:.2f}".format(linear_model.score(X_test, y_test)))
+    # accuracy for X_test
+    accuracy = svm_model.score(data['test']['X'], data['test']['y'])
+    print('Accuracy of SVM classifier on test set: {:.2f}'.format(accuracy))
+    run.log('Accuracy', np.float(accuracy))
+    
+    # precision for X_test
+    precision = precision_score(svm_predictions, data["test"]["y"], average='weighted')
+    print('Precision of SVM classifier on test set: {:.2f}'.format(precision))
+    run.log('precision', precision)
+    
+    # recall for X_test
+    recall = recall_score(svm_predictions, data["test"]["y"], average='weighted')
+    print('Recall of SVM classifier on test set: {:.2f}'.format(recall))
+    run.log('recall', recall)
+    
+    # f1-score for X_test
+    f1 = f1_score(svm_predictions, data["test"]["y"], average='weighted')
+    print('F1-Score of SVM classifier on test set: {:.2f}'.format(f1))
+    run.log('f1-score', f1)
+    
+    # create a confusion matrix
+    labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    labels_numbers = [0, 1, 2]
+    cm = confusion_matrix(y_test, svm_predictions, labels_numbers)
+    log_confusion_matrix(cm, labels)
     
     # files saved in the "outputs" folder are automatically uploaded into run history
-    model_file_name = "model.pkl"
-    joblib.dump(linear_model, os.path.join('outputs', model_file_name))
+    joblib.dump(svm_model, os.path.join('outputs', args.modelname))
+    run.log('Model Name', np.str(args.modelname))
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--kernel', type=str, default='rbf', help='Kernel type to be used in the algorithm')
     parser.add_argument('--penalty', type=float, default=1.0, help='Penalty parameter of the error term')
+    parser.add_argument('--modelname', type=str, default='model.pkl', help='Name of the model file')
     args = parser.parse_args()
     return args
 
